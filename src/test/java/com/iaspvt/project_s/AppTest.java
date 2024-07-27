e53151e45bc50ad83762dcf61e465c134ce9d29f
saikumar0503 <saikumarbalaga03@gmail.com>
2024-07-27 16:21:44 +0530

    Update AppTest.java

--- src/test/java/com/iaspvt/project_s/AppTest.java
+++ src/test/java/com/iaspvt/project_s/AppTest.java
@@ -1,13 +1,11 @@
-package com.iaspvt.project_s;
+package com.iaspvt.project_z;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 public class AppTest {
-    
-  @Test
-  public void shouldAnswerWithTrue() {
-    assertTrue(true);
-  }
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
 }
